faf2a9cf1011b4b1388b8fc0de2c2d95d642c75e
lucalewin <luca.lewin23@gmail.com>
2021-08-11 13:50:42 +0200

    create basic structure

--- src/main/java/net/lucraft/featurefinder/FeatureFinder.java
+++ src/main/java/net/lucraft/featurefinder/FeatureFinder.java
@@ -1,5 +1,7 @@
 package net.lucraft.featurefinder;
 
+import net.lucraft.featurefinder.util.Position;
+
 public class FeatureFinder {
 
     private final long seed;
@@ -8,18 +10,31 @@ public class FeatureFinder {
         this.seed = seed;
     }
 
-    public long findFeature(Feature feature) {
-        long pos = 0;
+    public Position findFeature(Feature feature) {
+        Position pos = null;
         switch (feature) {
+            case SLIME_CHUNK -> pos = findSlimeChunk();
+            case AMETHYST_GEODE -> pos = findAmethystGeode();
             case WITCH_HUD -> pos = findWitchHud();
+            case QUAD_WITCH_HUD -> pos = findQuadWitchHud();
         }
         return pos;
     }
 
+    private Position findSlimeChunk() {
+        return null;
+    }
 
+    private Position findAmethystGeode() {
+        return null;
+    }
 
-    private long findWitchHud() {
-        return 0;
+    private Position findWitchHud() {
+        return null;
+    }
+
+    private Position findQuadWitchHud() {
+        return null;
     }
 
     public long getSeed() {
