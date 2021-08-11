86127f6ae9da93543e03e91327003f9259a2155a
lucalewin <luca.lewin23@gmail.com>
2021-08-11 12:47:38 +0200

    add helper method for Position conversion

---
+++ src/main/java/net/lucraft/featurefinder/util/math/MathHelper.java
@@ -0,0 +1,30 @@
+package net.lucraft.featurefinder.util.math;
+
+public class MathHelper {
+
+    private static final int[] MULTIPLY_DE_BRUIJN_BIT_POSITION = new int[]{0, 1, 28, 2, 29, 14, 24, 3, 30, 22, 20, 15, 25, 17, 4, 8, 31, 27, 13, 23, 21, 19, 16, 7, 26, 12, 18, 6, 11, 5, 10, 9};
+
+    public static int smallestEncompassingPowerOfTwo(int value) {
+        int i = value - 1;
+        i |= i >> 1;
+        i |= i >> 2;
+        i |= i >> 4;
+        i |= i >> 8;
+        i |= i >> 16;
+        return i + 1;
+    }
+
+    public static boolean isPowerOfTwo(int value) {
+        return value != 0 && (value & value - 1) == 0;
+    }
+
+    public static int log2DeBruijn(int value) {
+        value = isPowerOfTwo(value) ? value : smallestEncompassingPowerOfTwo(value);
+        return MULTIPLY_DE_BRUIJN_BIT_POSITION[(int)((long)value * 125613361L >> 27) & 31];
+    }
+
+    public static int log2(int value) {
+        return log2DeBruijn(value) - (isPowerOfTwo(value) ? 0 : 1);
+    }
+
+}
